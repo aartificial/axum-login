0cbb622e0091988b75dc6d035fd859f7f20f1de9
sol <sol.recieve@gmail.com>
2023-10-25 19:01:38 +0200

    wip

---
+++ axum-graphql/src/model/mod.rs
@@ -0,0 +1,17 @@
+use async_graphql::{Context, Object, Schema};
+use async_graphql::{EmptyMutation, EmptySubscription};
+
+pub(crate) type ServiceSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
+
+pub(crate) struct QueryRoot;
+
+#[Object]
+impl QueryRoot {
+    async fn hello(&self, _ctx: &Context<'_>) -> &'static str {
+        "Hello world"
+    }
+
+    async fn add(&self, a: i32, b: i32) -> i32 {
+        a + b
+    }
+}
